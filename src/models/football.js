0afbc97f027afde79194e7c1f77826da5c302c81
wahyukhairina <wkhairina.afzan@gmail.com>
2020-06-06 09:08:58 +0800

    eslint clean code

--- src/models/football.js
+++ src/models/football.js
@@ -3,38 +3,39 @@ const connection = require('../config/mysql')
 module.exports = {
     insertScore: (data) => {
         return new Promise((resolve, reject) => {
-            for (let i = 0; i < 2 ; i ++) {
+            for (let i = 0; i < 2; i++) {
                 let name = data.clubname[i];
                 let points = data.clubpoints[i];
-            connection.query (`SELECT * FROM tb_club WHERE club_name = '${name}'`, (error, result) => {
-              console.log(result)
-                if (result[0] !== undefined) {
-                    connection.query(
-                        `UPDATE tb_club SET club_points = ${result[0].club_points + points} WHERE club_name = '${name}'`, (error, result) => {
-                            // console.log(error, 'error')
-                            // console.log(result, 'result')
-                            if(error) reject ( new Error (error));
-                            resolve(result);
-                        }
-                    );
-                } else {
-                    connection.query('ALTER TABLE tb_club AUTO_INCREMENT = 0');
-                    connection.query(
-                        `INSERT INTO tb_club (club_name, club_points) VALUES ( '${name}', ${points})`, (error, result) => {
-                            if (error) reject(new Error(error));
-                            resolve(result);
-                        }
-                    )
-                }
-            } )
+                connection.query(`SELECT * FROM tb_club WHERE club_name = '${name}'`, (error, result) => {
+                    console.log(result)
+                    if (result[0] !== undefined) {
+                        connection.query(
+                            `UPDATE tb_club SET club_points = ${result[0].club_points + points} WHERE club_name = '${name}'`, (error, result) => {
+                                // console.log(error, 'error')
+                                // console.log(result, 'result')
+                                if (error) reject(new Error(error));
+                                resolve(result);
+                            }
+                        );
+                    } else {
+                        connection.query('ALTER TABLE tb_club AUTO_INCREMENT = 0');
+                        connection.query(
+                            `INSERT INTO tb_club (club_name, club_points) VALUES ( '${name}', ${points})`, (error, result) => {
+                                if (error) reject(new Error(error));
+                                resolve(result);
+                            }
+                        )
+                    }
+                })
             }
-          })},
+        })
+    },
     getAll: () => {
         return new Promise((resolve, reject) => {
             connection.query(
                 'SELECT club_name as clubname, club_points as points FROM tb_club ORDER BY club_points DESC', (error, result) => {
-                   console.log(result, 'result')
-                    if(error) reject(new Error(error));
+                    console.log(result, 'result')
+                    if (error) reject(new Error(error));
                     resolve(result);
                 }
             )
